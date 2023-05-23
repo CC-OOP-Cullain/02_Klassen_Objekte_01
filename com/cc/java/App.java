55560321852628080db6bc5b61a64eddfc63dd2e
Peter <schmidt.multimedia@gmail.com>
2023-05-23 11:30:18 +0200

    Custom Constructor

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,31 +5,17 @@ public class App {
     public static void main(String[] args) {
       
         /* Instanziierung Klasse --> Objekt */
-        // Cat -- Referenztyp
-        // cat -- Referenzvariable
-        
-        Cat cat1 = new Cat();
-        
-        // Wertzuweisung
-        cat1.name = "Grizabella";
-        cat1.furColor = "white";
-        cat1.age = 29;
-
+        Cat cat1 = new Cat("Grizabella", "white", 29);
+    
         // Ausgabe über Methoden
         output(cat1.tellYourName());
         output(cat1.tellYourFurColor());
         output(Integer.toString(cat1.tellYourAge()));
 
-        // App.output("hi");
         output("-------------------");
 
-        Cat cat2 = new Cat();
+        Cat cat2 = new Cat("Alonzo","grey",35);
        
-        // Wertzuweisung
-        cat2.name = "Alonzo";
-        cat2.furColor = "grey";
-        cat2.age = 35;
-
         // Ausgabe direkt
         output(cat2.name);
         output(cat2.furColor);
